97418c13780101cf0fd4077461fc75e68bef7e72
Runtang Wang <rwang@mrn.org>
2017-08-22 17:45:59 -0600

    add more recipient emails

--- getMessage.php
+++ getMessage.php
@@ -7,6 +7,6 @@ $message = 'From: ' . $name . "\n" .
     'Email: ' . $email . "\n" .
     'Message: ' . "\n" . $_POST['message'];
 
-mail('rt2k101@gmail.com', 
+mail('rt2k101@gmail.com,dryflysandy@comcast.net,jeraldcross@comcast.net', 
     'User Message From Cancersupportnow.org',
     $message);
