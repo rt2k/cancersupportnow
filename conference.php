32b3f68c03bab7ec1ebbd02672c434882bd0aad4
Runtang Wang <rwang@mrn.org>
2019-03-30 15:59:10 -0600

    added conference pics and ppt

--- conference.php
+++ conference.php
@@ -8,14 +8,49 @@
 <table>
 <tr><td>Date/Time:</td><td>March 23rd, 2019, 8:30am to 4:00pm</td></tr>
 <tr><td valign='top'>Location:</td><td>Central United Methodist Church<br/>201 University Blvd NE, Albuquerque, NM</td></tr>
-<tr><td>Registration:</td><td><a href='#' onclick="window.open('https://docs.google.com/forms/d/e/1FAIpQLScj0SKqH8A4v6_zjiKD2A100JIaMxZ-IKjV0M2VhBkkbJ0eHQ/viewform');">Register online</a>
-OR <a href='#' onclick='window.open("pdfs/2019_conference_registration.pdf");'>download</a> the registration form, complete it and mail to us</a>
+<tr><td>Registration:</td><td><i>expired</i><!--a href='#' onclick="window.open('https://docs.google.com/forms/d/e/1FAIpQLScj0SKqH8A4v6_zjiKD2A100JIaMxZ-IKjV0M2VhBkkbJ0eHQ/viewform');">Register online</a>
+OR <a href='#' onclick='window.open("pdfs/2019_conference_registration.pdf");'>download</a> the registration form, complete it and mail to us</a-->
 </td></tr>
 </table>
 <br/>
 
 
 <br/>
+
+The 8th annual Cancer Support Now Living with and Beyond Cancer Conference was a great success.  Thank you to all who attended, and special thanks to our presenters and volunteer helpers. See the next newsletter and/or website for pictures.
+<br/><br/>
+Here are the slideshow handouts from our March 23 Living With and Beyond Cancer speakers and breakout presentations, which we were given permission to post and share.
+<br/><br/>
+Your Pain and how to Manage it (Chronic Pain Self Management Program) had no handouts, but offers various FREE Cancer Thriving and Surviving 6 week workshops thru the year at varying sites and times.  Keep an eye on our website, facebook page, newsletters, emails, or call the Helpline to find out when the next sessions are coming. Great for survivors and caregivers.
+<br/><br/>
+Click on the links below to see their PowerPoints.
+ <br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_integrativeMedicine.pdf");'>Integrative Medicine</a> by Dr. Surya Pierce
+<br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_clinicalTrials.pdf");'>Clinical Trials</a> by Ellen Wojcik
+<br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_aiChi.pdf");'>Ai Chi</a> by Renee Marshall
+<br/><br/>
+CBD and Medical Cannabis by Gina Lucero and Marissa Cecco had 3 slide shows:
+<br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_cannabis.pdf");'>How Cannabis can Help</a>              <a href='#' onclick='window.open("pdfs/8th_conference_pp_endocannabinoid.pdf");'>The Endocannabinoid System</a>           <a href='#' onclick='window.open("pdfs/8th_conference_pp_cbd.pdf");'>CBD P450</a>
+ <br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_palliativeCare.pdf");'>Palliative Care</a>   by Dr. Kathy Morsea
+  <br/><br/>
+<a href='#' onclick='window.open("pdfs/8th_conference_pp_intimacy.pdf");'>Intimacy and Relationships</a> by Tera S Jones, PhD
+<br/><br/>
+She also showed an interesting 2 minute video called <a href='https://www.bing.com/videos/search?view=detail&mid=75E46C3FBD37D4166BD875E46C3FBD37D4166BD8&shtp=GetUrl&shid=053d7c1f-5625-46c6-b36e-a153928c2f0e&shtk=SXQncyBOb3QgQWJvdXQgVGhlIE5haWw%3D&shdk=IkRvbid0IHRyeSB0byBmaXggaXQuIEkganVzdCBuZWVkIHlvdSB0byBsaXN0ZW4uIiBFdmVyeSBtYW4gaGFzIGhlYXJkIHRoZXNlIHdvcmRzLiBBbmQgdGhleSBhcmUgdGhlIGxhdyBvZiB0aGUgbGFuZC4gTm8gbWF0dGVyIHdoYXQu&shhk=MSs6Hh%2BJ0g%2BgPG97yjGquqal09Y0Z/mZca7qmP80G3E%3D&form=VDSHOT&shth=OSH.GfbaeHthl9K3kTmX3pIsRw' target='_blank'>It’s Not About the Nail</a>
+
+<br/><br/>
+
+<p>Conference pictures:</p>
+<img src="pdfs/8th_conference_pic1.jpg" height="50%"/>
+<img src="pdfs/8th_conference_pic2.jpg" height="50%"/>
+<img src="pdfs/8th_conference_pic3.jpg" height="50%"/>
+<img src="pdfs/8th_conference_pic4.jpg" height="50%"/>
+<img src="pdfs/8th_conference_pic5.jpg" height="50%"/>
+<br/><br/>
+<!--
 Opening Speaker: <a href='#' onclick='window.open("pdfs/2019_conference_surya-2.pdf")'>Integrative Medicine</a> – Surya Pierce, MD
 <br/><br/>
 Morning Breakout Sessions:
@@ -35,28 +70,9 @@ Afternoon Breakout Sessions
 <li><a href='#' onclick='window.open("pdfs/2019_conference_painflyer.pdf")'>Pain Management</a> - Catherine A. Offutt &amp; Patricia Torn, Chronic Disease Self-Management Education Programs
 <li><a href='#' onclick='window.open("pdfs/2019_conference_imitacy_relationship.pdf")'>Intimacy and Relationships</a> - Tera S. Jones, PhD
 </ul>
-<!--a href='#' onclick='window.open("pdfs/6th_conference_program.pdf");'>Conference program</a><br/><br/-->
- 
-<!-- Conference Talks/Materials: -->
-<!--a href='#' onclick='window.open("pdfs/6th_conference_gail_rubin.pdf");'>Gail Rubin</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_lymphedema_talk.pdf");'>Lymphedemo Talk</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_verdes_foundation.pdf");'>Verdes Foundation</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_sacredness_healing.pdf");'>Sacredness in Healing</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_mind_body_neutral.pdf");'>Mind-body neutral background</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_new_mexico_medical_cannabis_program.pdf");'>New mexico medical cannabis program</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_nmmcp_patient_statistics0217.pdf");'>NMMCP patient statistics-Feb-2017</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_licensed_nonprofit_producers.pdf");'>Licensed Nonprofit producers</a>&nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_medical_cannabis_patient_application.pdf");'>Medical cannabis patient application</a>
-<br/-->
-<!--p>Conference Fliers: <a href='#' onclick='window.open("pdfs/6th_conference_lymphedema.pdf");'>Lymphedema</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_improvmedicine.pdf");'>Improv Medicine</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_hospice101.pdf");'>Hospice 101</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_vialoflife.pdf");'>Vial of life</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_jmflyer.pdf");'>JM Lunchtime speaker</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_medicalcannabisflyer.pdf");'>Medical cannabis</a> &nbsp;
-<a href='#' onclick='window.open("pdfs/6th_conference_kwlflyer.pdf");'>Sacredness in Healing</a> &nbsp;( more to come ...)</p-->
-<!--p>Event schedule available <a href='#' onclick='window.open("pdfs/4th_conference_3_28_2015_schedule.pdf");'>here</a>.</p-->
 
+-->
+ 
 
 <br/><br/>
 <fieldset>
